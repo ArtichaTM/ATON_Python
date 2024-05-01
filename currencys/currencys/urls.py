485dcc0b5d197b64b7ce0cbd8c2b849c0ae996ad
Articha <tima-1324@mail.ru>
2024-05-01 13:10:29 +0300

    Added redirect from / to /currency/

--- currencys/currencys/urls.py
+++ currencys/currencys/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(
+        pattern_name='currency:index',
+        permanent=False)
+    ),
     path("admin/", admin.site.urls),
     path('currency/', include('currency.urls'))
 ]
